48d66251e612b8aa1bba81cd0b0649be7f0d8ba6
axuinflx <Allen@ITs-MacBook-Pro-2.local>
2015-07-30 17:10:44 -0400

    Dismiss KB on touch for PIN Login page

--- Headzup/PinViewController.swift
+++ Headzup/PinViewController.swift
@@ -127,15 +127,8 @@ class PinViewController: UIViewController,ValidationDelegate, UITextFieldDelegat
         println("Validation FAILED!")
     }
 
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        self.view.endEditing(true)
     }
-    */
 
 }
