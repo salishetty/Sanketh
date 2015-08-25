16fecf4b6e6ed7c6ad2cff99b4398037fc53097a
Sandeep Menon <smenon@inflexxion.com>
2015-08-25 16:18:10 -0400

    Added Title bad and view controllers

---
+++ Headzup/TrackViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TrackViewController.swift
+//  Headzup
+//
+//  Created by Sandeep Menon Ayyappankutty on 8/25/15.
+//  Copyright (c) 2015 Inflexxion. All rights reserved.
+//
+
+import UIKit
+
+class TrackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
