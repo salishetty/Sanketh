6fe2cd2a4287bf4fc651ec0b6c5b058120346f6b
ahworeta <aworeta@sandeep-mac.local>
2015-10-02 17:04:43 -0400

    Updated SynchHelper with a couple of other Synch functions

--- Headzup/GeneralHelper.swift
+++ Headzup/GeneralHelper.swift
@@ -10,25 +10,6 @@ import Foundation
 
 public class GeneralHelper
 {
-    init ()
-    {
-    }
-    func userActionItemsToDictionary(uaItems: UserActionItems) -> [String:String]
-    {
-        return [UserActionKeys.MembershipUserID:uaItems.membershipUserId, UserActionKeys.DeviceID:uaItems.deviceId, UserActionKeys.DeviceType:uaItems.deviceType, UserActionKeys.ActionDate:uaItems.actionDate as String, UserActionKeys.OsVersion:uaItems.osVersion, UserActionKeys.AppVersion:uaItems.appVersion, UserActionKeys.ActionType:uaItems.actionType, UserActionKeys.Comment:uaItems.comment]
-    }
-    func technicalLogItemsToDictionary(tLogItems: TechnicalLogItems) -> [String:String]
-    {
-        return [TechnicalLogKeys.Message:tLogItems.message, TechnicalLogKeys.DeviceID:tLogItems.deviceId, TechnicalLogKeys.Exception:tLogItems.exception, TechnicalLogKeys.ModuleName:tLogItems.moduleName, TechnicalLogKeys.EventDate:tLogItems.eventDate as String, TechnicalLogKeys.AppVersion:tLogItems.appVersion, TechnicalLogKeys.OSVersion:tLogItems.osVersion, TechnicalLogKeys.LogLevel:tLogItems.logLevel]
-    }
-    func favoriteItemsToDictionary(favoriteItems: FavoriteItems) -> [String:String]
-    {
-        return [FavoriteKeys.MembershipUserID:favoriteItems.membershipUserId, FavoriteKeys.GroupType:favoriteItems.groupType.stringValue, FavoriteKeys.ContentID:favoriteItems.contentID.stringValue, FavoriteKeys.IsActive:favoriteItems.isActive]
-    }
-    public static func responseItemsToDictionary(responseItems: ResponseItems) -> [String:String]
-    {
-        return [AboutMeResponseKeys.MembershipUserID:responseItems.membershipUserId, AboutMeResponseKeys.QuestionID:responseItems.questionID, AboutMeResponseKeys.ResponseValue:responseItems.responseValue, AboutMeResponseKeys.DateAdded:responseItems.dateAdded as String]
-    }
     public static func convertDateToString(date:NSDate)->String
     {
         let dateFormatter = NSDateFormatter()
